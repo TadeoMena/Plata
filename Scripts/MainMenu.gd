c7d29bb03f63bc74ba71944b061193da2c83b701
JosiasAlvaredo <135233972+JosiasAlvaredo@users.noreply.github.com>
2025-03-31 10:17:54 -0300

    Prueben

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,13 +1,23 @@
-extends Control
+extends Node2D
 
-# Función para el botón "Jugar"
-func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/GameSelection.tscn")
+var scenes = {
+	"Jugar": "res://scenes/GameSelection.tscn",
+	"Historias": "res://scenes/Stories.tscn",
+	"Inicio sesion": "res://scenes/Login.tscn"
+}
 
-# Función para el botón "Historias"
-func _on_stories_pressed():
-	get_tree().change_scene_to_file("res://scenes/Stories.tscn")
+func _ready():
+	var container = $HBoxContainer  
+	for button_name in scenes.keys():
+		var button = container.get_node_or_null(button_name)  
+		if button:
+			button.pressed.connect(_on_button_pressed.bind(button_name))  
+		else:
+			print("Error: No se encontró el botón", button_name)
 
-# Función para el botón "Login"
-func _on_login_pressed():
-	get_tree().change_scene_to_file("res://scenes/Login.tscn")
+func _on_button_pressed(button_name):
+	var scene_path = scenes.get(button_name, "")
+	if scene_path:
+		get_tree().change_scene_to_file(scene_path)
+	else:
+		print("Error: No se encontró la escena para", button_name)
